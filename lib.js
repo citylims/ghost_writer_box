29549fa51839ce4bc2ae2ad97c59965e866adeb3
Ausin Formica <austin.formica@gmail.com>
2015-08-02 19:00:47 -0400

    setInteral text

--- lib.js
+++ lib.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
-  var background = document.querySelector("#album");
+  var canvas = document.querySelector("#album");
   var foreground = document.getElementById('top-layer');
-  var ctx = background.getContext("2d");
+  var ctx = canvas.getContext("2d");
   var ctx2 = foreground.getContext("2d");
   var timer, fullText, currentOffset, onComplete, wordSet;
-
   var green = "#548779";
   var black = "#1D1F21";
+  ctx.translate(0.5, 0.5);
+  canvas.width = 1000;
+  canvas.height = 500;
+  canvas.style.width = canvas.width;
+  canvas.style.height = canvas.height;
+
+  init();
 
 
   function drawBubble(x,y) {
@@ -17,79 +23,55 @@ $(document).ready(function() {
     ctx.rect(x, y, 600, 300)
     ctx.fillStyle = gradient;
     ctx.fill();
-    ctx.lineWidth = .4;
-    ctx.strokeStyle = gradient;
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#c0c0c0";
     ctx.stroke();
   }
 
-var person = "Squall";
-var text = "Taken...angel's...singing  voices...zeno...gias";
-
-  drawBubble(80, 100);
-  writeText(person, text);
-
-
+  function init() {
+    var person = "Squall";
+    var text = "Taken...angel's...singing voices...zeno...gias";
+    drawBubble(80, 100);
+    writeText(person, text);
+  }
 
   function writeText(person, text) {
-    var name = person
     fullText = text;
     wordSet = text.split('');
     currentOffset = 0;
     timer = setInterval(onTick, 100);
-
-    // for (var i = 0; arr.length > i; i++) {
-    //
-    // }
-
+    dialogHeader(person)
   }
-
 
   function onTick() {
-      currentOffset++;
-      if (currentOffset == wordSet.length) {
-          complete();
-          return;
-      }
-      var text = "";
-      var x = 100;
-      var y = 120;
-      var pad = 15;
-      for(var i = 0; i < currentOffset; i++){
-       text += wordSet[i] + "";
-       ctx.font = "15px Arial";
-       ctx.fillStyle = 'black';
-       ctx.fillText(wordSet[i], x, y);
-       x += pad;
-      //  y += pad;
-      }
-      text.trim();
-
-  }
-
-  function complete() {
+    currentOffset++;
+    if (currentOffset == wordSet.length) {
+      ctx.closePath();
       clearInterval(timer);
-      timer = null;
-      $("#message").html(fullText);
-      if (onComplete) onComplete();
+      return;
+    }
+    var text = "";
+    var x = 100;
+    var y = 200;
+    var pad = 15;
+    for (var i = 0; i < currentOffset; i++){
+      if (x  > 600) {
+        y += 25;
+        x = 100;
+      }
+      text += wordSet[i] + "";
+      ctx.font = "15px Arial";
+      ctx.fillStyle = 'white';
+      ctx.fillText(wordSet[i], x, y);
+      x += pad;
+    }
+    text.trim();
   }
 
-  $(".box").click(function () {
-      complete();
-  });
+  function dialogHeader(name) {
+    ctx.font = "25px Arial"
+    ctx.fillStyle = 'white';
+    ctx.fillText(name, 100, 150);
+  }
 
-  var title = "Tifa";
-  var text = "Taken...angel's...singing  voices...zeno...gias";
-
-  $('.speech-button').on("click", function() {
-    Speak(title, text, function () {
-      setTimeout(function () {
-        Speak("good bye", "lover")
-      }, 2000);
-    });
-  });
-
-
-
-
-
-})
+})//init
