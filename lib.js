02a69cb6bc36fedf02abe1608b16181ce72dff1f
Ausin Formica <austin.formica@gmail.com>
2015-08-02 22:18:09 -0400

    dialog cycle logic

--- lib.js
+++ lib.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var foreground = document.getElementById('top-layer');
   var ctx = canvas.getContext("2d");
   var ctx2 = foreground.getContext("2d");
-  var timer, fullText, currentOffset, onComplete, wordSet;
+  var timer, fullText, currentOffset, onComplete, wordSet, chatCount, multiLength;
   var cloud = document.getElementById("cloudImg");
   var green = "#548779";
   var black = "#1D1F21";
@@ -30,35 +30,41 @@ $(document).ready(function() {
 
   function init() {
     var person = "Cloud";
-    var dialog = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    dialog = dialog.split('');
-    console.log(dialog.length)
-    // var textCalc = text.split('');
+    var text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    dialog = text.split('');
+    console.log(dialog.length);
     if (dialog.length > 100) {
       var dialog = textCal(dialog)
       drawBubble(80, 100);
+      writeText(person, dialog)
     }
     else {
       drawBubble(80, 100);
-      writeText(person, text);
+      writeText(person, dialog);
     }
   }
 
 
   function textCal(dialog) {
-    var someArray = dialog;
-    var groupSize = 50;
-    var cycle = _.map(someArray, function(item, index){
-    return index % groupSize === 0 ? someArray.slice(index, index + groupSize) : null;
+    var limit = 50;
+    var cycle = _.map(dialog, function(item, index){
+    return index % limit === 0 ? dialog.slice(index, index + limit) : null;
     });
-    var groups = cycle.filter(function(item){ return item;});
-    console.log(groups);
-    return groups
-}
+    var collection = cycle.filter(function(item){ return item;});
+    console.log(collection);
+    return collection
+  }
 
   function writeText(person, text) {
-    fullText = text.join('');
     console.log(text.length);
+    if (chatCount === undefined) {
+      chatCount = 0;
+    }
+    if (text.length > 1) {
+      multiLength = text.length
+      wordSet = text[chatCount];
+    }
+    fullText = text.join('');
     currentOffset = 0;
     timer = setInterval(onTick, 100);
     dialogHeader(person)
@@ -66,16 +72,25 @@ $(document).ready(function() {
 
   function onTick() {
     currentOffset++;
-    if (currentOffset == text.length) {
+    if (multiLength === chatCount) {
       ctx.closePath();
       clearInterval(timer);
+      return
+    }
+    if (currentOffset == wordSet.length) {
+      ctx.closePath();
+      clearInterval(timer);
+      if (multiLength !== chatCount) {
+        console.log("next array")
+        drawBubble(80, 100);
+      }
       return;
     }
     var text = "";
     var x = 100;
     var y = 210;
     var pad = 15;
-    for (var i = 0; i < currentOffset; i++){
+    for (var i = 0; i < currentOffset + 1; i++){
       if (x  > 600) {
         y += 25;
         x = 100;
